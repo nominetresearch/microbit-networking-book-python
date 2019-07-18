b5a35520596cfe9a238aa2f7143353e4b4f20803
JGrout <32357909+JGrout@users.noreply.github.com>
2019-07-18 11:57:48 +0100

    Add files via upload

---
+++ Broadcast_Communications_Task 1.py
@@ -0,0 +1,10 @@
+from microbit import *
+import radio
+
+# Turns radio on - this must be stated in the code
+radio.on()
+
+# The line below can be used to change the radio group,
+# it is already set to 0 by default but can be reconfigured to a number from 0-255
+
+#radio.config(group = 0)
