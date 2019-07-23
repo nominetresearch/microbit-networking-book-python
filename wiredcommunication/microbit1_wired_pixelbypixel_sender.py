4348e629a93b0a4572b916b000babff84cbf3d99
Cigdem Sengul <cigdem.sengul@nominet>
2019-07-23 15:29:33 +0100

    Adding pixel by pixel sending

---
+++ wiredcommunication/microbit1_wired_pixelbypixel_sender.py
@@ -0,0 +1,33 @@
+from microbit import *
+import microbit
+
+# 2nd Micro:bit does not start with displaying a heart icon
+heart=[0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,0,0,1,0,0]
+x=0
+y=0
+start = False
+index=0
+
+# Do always
+
+while True:
+    if button_a.is_pressed():
+        display.show(Image.HEART)
+        start = True
+
+    if start:
+        index=0
+        while(index < 25):
+            if(heart[index]==1):
+                pin1.write_digital(1)
+                sleep(550)
+            else:
+                pin1.write_digital(1)
+                sleep(250)
+            pin1.write_digital(0)
+            sleep(100)
+            y=index//5
+            x=index%5
+            microbit.display.set_pixel(x, y, 0)
+            index=index+1
+        start=False
---
+++ wiredcommunication/microbit2_wired_pixelbypixel_receiver.py
@@ -0,0 +1,35 @@
+from microbit import *
+
+x=0
+y=0
+one_threshold = 400
+zero_threshold = 150
+
+while True:
+    signal_time = None
+
+    while(pin2.read_digital()):
+        if not signal_time:
+            signal_time = running_time()
+
+    line_free_time = running_time()
+
+    if(signal_time):
+        duration = line_free_time - signal_time
+
+        if(duration > one_threshold):
+            display.set_pixel(x,y,9)
+
+        if(duration > zero_threshold):
+            x=x+1
+            if(x>4):
+                x=0
+                y=y+1
+            if(y>4):
+                y=0
+                x=0
+        else:
+            pass
+
+    if button_a.is_pressed():
+        display.clear()
