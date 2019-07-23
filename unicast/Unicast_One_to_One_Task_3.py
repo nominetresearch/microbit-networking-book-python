6c9c5391889607b880f6a78c93d66f1030294893
JGrout <32357909+JGrout@users.noreply.github.com>
2019-07-23 12:15:09 +0100

    Add files via upload

---
+++ unicast/Unicast_One_to_One_Task_3.py
@@ -0,0 +1,22 @@
+from microbit import *
+import radio
+
+# Turns radio on - this must be stated in the code
+radio.on()
+
+# The line below can be used to change the radio group,
+# it is already set to 0 by default but can be reconfigured to a number from 0-255
+
+#radio.config(group = 0)
+
+# Create the header
+string_sender = "CS"
+string_receiver = "AK"
+header = string_sender + string_receiver
+
+# Create the message and attach to header to form package
+message = "Hello"
+packet = header + message
+
+# Send message
+radio.send(packet)
