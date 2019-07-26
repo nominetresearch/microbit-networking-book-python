c21f5715b3276018a4e5debca5ae610b27947478
JGrout <32357909+JGrout@users.noreply.github.com>
2019-07-26 13:30:02 +0100

    Update Wired_Communication_Task_2.py

--- wiredcommunication/code/Wired_Communication_Task_2.py
+++ wiredcommunication/code/Wired_Communication_Task_2.py
@@ -9,7 +9,8 @@ while True:
         sleep(100)
         pin1.write_digital(0) # Set pin1 to low
     
-    # If pin2 is read as high, turn on the led at (2, 2)    
+    # If pin2 is read as high, turn on the led at (2, 2)
+    # Otherwise turn off the led at (2, 2)
     signal = pin2.read_digital()
     if signal == 1:
         display.set_pixel(2, 2, 9) # Set led at (2,2) to a brightness of 9 (on)
