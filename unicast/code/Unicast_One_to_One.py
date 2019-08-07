52025832814a2c8cdc79f195bd5b442e55d45321
Cigdem Sengul <cigdem.sengul@nominet>
2019-08-07 17:33:24 +0100

    Unicast code review - todo list updated for fixes

--- unicast/code/Unicast_One_to_One.py
+++ unicast/code/Unicast_One_to_One.py
@@ -2,25 +2,21 @@ from microbit import *
 import radio
 
 ### TASK 1 ###
-
-# Turns radio on - this must be stated in the code
+# Turn radio on 
 radio.on()
 
 # The line below can be used to change the radio group,
-# it is already set to 0 by default but can be reconfigured to a number from 0-255
-
+# set to 0 by default but can be configured to  0-255
 #radio.config(group = 0)
 
 ### TASK 2 ###
-
-# Create the header
+# Create the head have symmetric headers 
 string_sender = "CS"
 string_receiver = "AK"
 header = string_sender + string_receiver
 
 ### TASK 3 ###
-
-# Create the message and attach to header to form package
+# Create the message and attach to header to form packet
 message = "Hello"
 packet = header + message
 
@@ -30,7 +26,7 @@ radio.send(packet)
 ### TASK 4 ###
 
 while True:
-    # Checks for messages, any of length 4 or greater will be kept
+    # Check for messages, any of length 4 or greater will be kept
     incoming = radio.receive()
     if incoming is not None:
         if len(incoming) >= 4:
